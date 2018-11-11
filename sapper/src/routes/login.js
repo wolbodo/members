356f96016eb010bf0fb0c9a050d70ef372db75d5
Dexter <dexter@dexterlabs.nl>
2018-11-12 00:40:15 +0100

    Fixed shit, added login permissions

--- sapper/src/routes/login.js
+++ sapper/src/routes/login.js
@@ -8,56 +8,64 @@ const token = createJWT({
   user_roles: [
     { role: { name: 'login'}}
   ]
-});
+}, 'login');
 
 
 const client = createClient('http://graphql/v1alpha1/graphql', token)
 import gql from 'graphql-tag';
 
 
-export async function post(req, res, next) {
 
-  const result = await client.query({
-    query: gql`
-      query($email: String) {
-        account_user(where:{name:{_eq:$email}}) {
-          email password
-          user_roles {
-            role {
-              name
+export async function post(req, res, next) {
+  try {
+    const result = await client.query({
+      query: gql`
+        query($email: String) {
+          account_user(where:{email:{_eq:$email}}) {
+            id email password
+            user_roles {
+              role {
+                name
+              }
             }
           }
         }
+      `,
+      variables: {
+        email: req.body.email
       }
-    `,
-    variables: {
-      email: req.body.email
+    })
+    const { account_user: [user, ] } = result.data
+
+    console.log("Got post in login:", req.body, user, token)
+    if (user) {
+      console.log("Here", req.body.password, user.password)
+
+      const passwordOk = await bcrypt.compare(req.body.password, user.password)
+        
+      if (passwordOk) {
+        res.writeHead(200, {
+          'Content-Type': 'application/json'
+        })
+        res.end(JSON.stringify({
+          jwt: createJWT(user)
+        }))
+        return
+      }
     }
-  })
-  const { user: [user, ] } = result.data
-
-  console.log("Got post in login:", req.body, user)
-  if (user) {
-
-    console.log("Here", req.body.password, user.password)
-
-    const passwordOk = await bcrypt.compare(req.body.password, user.password)
-      
-    if (passwordOk) {
-      res.writeHead(200, {
-        'Content-Type': 'application/json'
-      })
-      res.end(JSON.stringify({
-        jwt: createJWT(user)
-      }))
-      return
-    }
+    res.writeHead(401, {
+      'Content-Type': 'application/json'
+    })
+    res.end(JSON.stringify({
+      error: 'Authentication failed'
+    }))
+  } catch (e) {
+    console.error('Hmm', e)
+    res.writeHead(500, {
+      'Content-Type': 'application/json'
+    })
+    res.end(JSON.stringify({
+      error: e.toString()
+    }))
   }
-
-  res.writeHead(401, {
-    'Content-Type': 'application/json'
-  })
-  res.end(JSON.stringify({
-    error: 'Authentication failed'
-  }))
 }
