a81e15bf6eb17f04f9a1c990fa01edddc457fe42
Dexter <dexter@dexterlabs.nl>
2018-12-05 23:29:46 +0100

    Added logout, and usersStore

--- sapper/src/stores/index.js
+++ sapper/src/stores/index.js
@@ -3,9 +3,10 @@
 import { Store as BaseStore } from 'svelte/store.js';
 
 import graphqlStore from './graphql'
+import usersStore from './users'
 
 
 export default function createStore(data) {
-  const Store = graphqlStore(BaseStore)
+  const Store = usersStore(graphqlStore(BaseStore))
   return new Store(data)
 }
---
+++ sapper/src/stores/users.js
@@ -0,0 +1,5 @@
+
+
+export default BaseStore =>
+  class Store extends BaseStore {
+  }
