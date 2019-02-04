7a70c0e2940280722fdeee151e2bb266a0772932
Dexter <dexter@dexterlabs.nl>
2019-02-04 10:49:29 +0100

    Using more roles for actions

--- sapper/src/stores/members.js
+++ sapper/src/stores/members.js
@@ -4,43 +4,38 @@ export default BaseStore =>
   class Store extends BaseStore {
     async addMember(member) {
       const {graphqlClient} = this.get();
-      try {
-        return await graphqlClient.mutate({
-          mutation: gql`
-          mutation (
-            $name: String!,
-            $fullname: String!,
-            $email: String,
-            $phone: String,
-            $streetname: String,
-            $housenumber: numeric,
-            $housenumberaddon: String,
-            $zipcode: String,
-            $city: String,
-            $country: String
-          ) {
-            insert_member(objects: [{
-              name: $name
-              fullname: $fullname
-              email: $email
-              phone: $phone
-              streetname: $streetname
-              housenumber: $housenumber
-              housenumberaddon: $housenumberaddon
-              zipcode: $zipcode
-              city: $city
-              country: $country
-            }]) {
-              affected_rows
-              __typename
-            }
-          }`,
-          variables: member
-        })
-      } catch (error) {
-        console.log(error)
-        return error
-      }
+      return await graphqlClient.mutate({
+        mutation: gql`
+        mutation (
+          $name: String!,
+          $fullname: String!,
+          $email: String,
+          $phone: String,
+          $streetname: String,
+          $housenumber: numeric,
+          $housenumberaddon: String,
+          $zipcode: String,
+          $city: String,
+          $country: String
+        ) {
+          insert_member(objects: [{
+            name: $name
+            fullname: $fullname
+            email: $email
+            phone: $phone
+            streetname: $streetname
+            housenumber: $housenumber
+            housenumberaddon: $housenumberaddon
+            zipcode: $zipcode
+            city: $city
+            country: $country
+          }]) {
+            affected_rows
+            __typename
+          }
+        }`,
+        variables: member
+      })
     }
 
     async updateMember(id, data) {
