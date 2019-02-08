756678f6bde5f7856afb0c862edf5881c05b22fa
Dexter <dexter@dexterlabs.nl>
2019-02-08 19:28:29 +0100

    Adds get

--- sapper/src/routes/echo.js
+++ sapper/src/routes/echo.js
@@ -1,12 +1,19 @@
 
 
 export function post (req, res) {
-  console.log("Got echo call", req.headers)
+  console.log("Got echo post", JSON.stringify(req))
 
   res.writeHead(500, {
     'Content-Type': 'application/json'
   })
-  res.end(JSON.stringify({
-    headers: req.headers
-  }))
+  res.end(JSON.stringify(req))
+}
+
+export function get (req, res) {
+  console.log("Got echo post", JSON.stringify(req))
+
+  res.writeHead(500, {
+    'Content-Type': 'application/json'
+  })
+  res.end(JSON.stringify(req))
 }
