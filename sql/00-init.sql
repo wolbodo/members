4d4a4674a5a323f2285f6ba4665df43278f19d1a
Dexter <a.esselink@gmail.com>
2020-11-08 19:39:41 +0100

    Fixes table structure

--- sql/00-init.sql
+++ sql/00-init.sql
@@ -3,6 +3,8 @@ CREATE EXTENSION btree_gist;
 
 BEGIN;
 
+CREATE SCHEMA auth;
+
 create type auth.jwt_token as (
   role text,
   exp integer,
@@ -34,12 +36,14 @@ CREATE TABLE auth.person (
   created           TIMESTAMPTZ         DEFAULT NOW(), -- Adding a created cause validity might have to be reset
   modified          TIMESTAMPTZ         DEFAULT NOW(),
 
+  UNIQUE(id),
+
   CONSTRAINT is_email CHECK (email ~ '^[^@]+@([a-zA-Z0-9][a-zA-Z0-9-]*\.)+(xn--[a-zA-Z0-9-]{4,}|[a-zA-Z]{2,})$')
   -- CONSTRAINT is_iban  CHECK (bankaccount ~ '^[a-zA-Z]{2}[0-9]{2}[a-zA-Z0-9]{4}[0-9]{7}([a-zA-Z0-9]?){0,16}$')
 );
 CREATE INDEX ON auth.person (email);
 
-ALTER TABLE auth.person ENABLE ROW LEVEL SECURITY
+-- ALTER TABLE auth.person ENABLE ROW LEVEL SECURITY
 -- Users can edit their own data
 -- Bestuur can edit all users
 
@@ -55,43 +59,41 @@ CREATE ROLE member;
 CREATE ROLE admin;
 CREATE ROLE board;
 
-CREATE TABLE auth.role (
-  id          SERIAL,
-  name        VARCHAR(255)          UNIQUE NOT NULL
-);
-INSERT INTO auth.role (name) VALUES ('admin'), ('board');
-
 CREATE TABLE auth.person_role (
-  person_id   INTEGER   REFERENCES person (id),
-  role_id    INTEGER   REFERENCES role   (id),
+  person_id   INTEGER   NOT NULL REFERENCES auth.person(id),
+  role        VARCHAR   NOT NULL, --     INTEGER   REFERENCES auth.role   (id),
 
   validity    TSTZRANGE           DEFAULT TSTZRANGE(now(),NULL),
 
-  EXCLUDE USING gist (person_id WITH =, role_id WITH =, validity WITH &&)
+  EXCLUDE USING gist (person_id WITH =, role WITH =, validity WITH &&)
 );
-INSERT INTO auth.person_role (person_id, role_id)
-  SELECT auth.person.id, auth.role.id FROM (
+
+CREATE FUNCTION
+  auth.check_role_exists() RETURNS trigger AS $$
+BEGIN
+  IF NOT EXISTS (SELECT 1 FROM pg_roles AS r WHERE r.rolname = new.role) THEN
+    RAISE foreign_key_violation USING message =
+      'unknown database role: ' || new.role;
+    RETURN NULL;
+  END IF;
+  RETURN new;
+END
+$$ LANGUAGE plpgsql;
+
+CREATE CONSTRAINT TRIGGER ensure_user_role_exists
+  AFTER INSERT OR UPDATE ON auth.person_role
+  FOR EACH ROW
+  EXECUTE PROCEDURE auth.check_role_exists();
+
+
+INSERT INTO auth.person_role (person_id, role)
+  SELECT auth.person.id, alias.role FROM (
     VALUES
       ('Alice', 'admin'),
       ('Bob', 'board')
   ) ALIAS (name, role)
   JOIN auth.person ON auth.person.name = alias.name
-  JOIN auth.role ON auth.role.name = alias.role
 ;
-
--- INSERT INTO public.member_role
---   (member_id, role_id, validity)
--- SELECT public.member.id, public.role.id, '[now,)' FROM
---     (VALUES
---         ('admin@example.org', array['member','admin']),
---         ('dexter@example.org', array['member', 'board']),
---         ('dj@example.org', array['member']),
---         ('bwb@example.org', array['member'])
---     ) alias (email, role_names)
---     JOIN public.member ON public.member.validity @> NOW() AND public.member.email = alias.email
---     JOIN public.role ON public.role.validity @> NOW() AND public.role.name IN (SELECT unnest(alias.role_names));
-
-
 
 
 COMMIT;
