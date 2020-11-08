2c108120e61e6367fedd4e9708057b5bc3bfd7a9
HMBond <hubertusmariabond@gmail.com>
2020-11-08 18:52:44 +0100

    add button and css var theming

---
+++ app/src/components/common/button.svelte
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<style>
+  button {
+    padding: var(--button-padding);
+    background: var(--main-color);
+    color: var(--color-white);
+    font-weight: bolder;
+    border: none;
+  }
+</style>
+
+<button on:click>
+  <slot>Click</slot>
+</button>
