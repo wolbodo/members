dc5ec2b246d2451f4383e80709686b191ed569f8
Dexter <a.esselink@gmail.com>
2021-03-08 00:02:30 +0100

    Improving form

--- app/src/components/MemberView.svelte
+++ app/src/components/MemberView.svelte
@@ -59,45 +59,52 @@
 {:else}
   <h1>{$member.name}</h1>
 
-  <Sveltik initialValues={$member} {validate} {onSubmit} let:isSubmitting>
+  <Sveltik
+    initialValues={$member}
+    {validate}
+    {onSubmit}
+    let:isSubmitting
+    let:isDirty
+  >
     <Form class="form">
       <label for="name">Name:</label>
       <Field id="name" name="name" />
-      <ErrorMessage name="name" as="div" />
+      <ErrorMessage name="name" as="div" class="error" />
 
       <label for="email">Email:</label>
-      <Field id="email" name="email" />
-      <ErrorMessage name="email" as="div" />
+      <Field id="email" name="email" type="email" />
+      <ErrorMessage name="email" as="div" class="error" />
 
       <label for="phone">Phone:</label>
       <Field id="phone" name="phone" />
-      <ErrorMessage name="phone" as="div" />
+      <ErrorMessage name="phone" as="div" class="error" />
 
       <label for="zipcode">Zipcode:</label>
       <Field id="zipcode" name="zipcode" />
-      <ErrorMessage name="zipcode" as="div" />
+      <ErrorMessage name="zipcode" as="div" class="error" />
 
       <label for="address">Address:</label>
       <Field id="address" name="address" />
-      <ErrorMessage name="address" as="div" />
+      <ErrorMessage name="address" as="div" class="error" />
 
       <label for="city">City:</label>
       <Field id="city" name="city" />
-      <ErrorMessage name="city" as="div" />
+      <ErrorMessage name="city" as="div" class="error" />
 
       <label for="country">Country:</label>
       <Field id="country" name="country" />
-      <ErrorMessage name="country" as="div" />
+      <ErrorMessage name="country" as="div" class="error" />
 
       <label for="created">Created:</label>
       <Field id="created" name="created" />
-      <ErrorMessage name="created" as="div" />
+      <ErrorMessage name="created" as="div" class="error" />
 
       <label for="note">Note:</label>
       <Field id="note" name="note" />
-      <ErrorMessage name="note" as="div" />
+      <ErrorMessage name="note" as="div" class="error" />
 
-      <button type="submit" disabled={isSubmitting}>Submit</button>
+      <button type="submit" disabled={isSubmitting || !isDirty}>Submit</button>
+      <pre>{isDirty}</pre>
     </Form>
   </Sveltik>
 
@@ -110,6 +117,20 @@
     grid-template-columns: auto auto;
   }
 
+  :global(.form > .error) {
+    grid-column-end: span 2;
+  }
+
+  :global(.form input) {
+    border: none;
+    background: #ffa5002e;
+    font-size: 1.2rem;
+    padding: 0.3rem;
+    color: white;
+    border-bottom: 1px solid orange;
+  }
+
+  h1,
   label {
     color: white;
   }
