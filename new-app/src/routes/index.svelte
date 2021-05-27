e6820ddec662d9fbc78e5a498994245ff2671059
Dexter <a.esselink@gmail.com>
2021-05-27 22:51:04 +0200

    Setup graphql and svelte-kit

---
+++ new-app/src/routes/index.svelte
@@ -0,0 +1,73 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang='ts'>
+	import { client, user } from '$lib/graphql'
+	import { gql } from 'graphql-request'
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<section>
+	<h1>
+		<div class="welcome">
+			<picture>
+				<source srcset="svelte-welcome.webp" type="image/webp" />
+				<img src="svelte-welcome.png" alt="Welcome" />
+			</picture>
+		</div>
+
+		to the new<br />Members list
+	</h1>
+
+	{#if $user}
+		{#await client.request(gql`
+			{
+				auth_person{
+					name email
+				}
+			}
+		`)}
+			<p>Loading</p>
+		{:then data} 
+			<pre>{JSON.stringify(data, null, 2)}</pre>
+		{/await}
+	{:else}
+		<h2>
+			try logging in <a href='/login'>here</a>
+		</h2>
+	
+	{/if}
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
---
+++ new-app/src/routes/login.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { enhance } from '$lib/form'
+</script>
+
+<form
+  action="/login"
+  method="post"
+  use:enhance={{
+    result: async (res, form) => {
+      const created = await res.json();
+      todos = [...todos, created];
+
+      form.reset();
+    }
+  }}
+>
+  <label for='name'>Name</label>
+  <input id='name' name='name' placeholder="Use your nickname" />
+
+  <label for='password'>Password</label>
+  <input id='password' name='password' type='password' />
+
+  <button type='submit'>Submit</button>
+</form>
