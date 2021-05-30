494390ec1f244e44a775888e4e0bf3b5e8baef4c
Dexter <a.esselink@gmail.com>
2021-05-30 13:51:27 +0200

    Setup Github Actions and deployment

--- app/src/hooks.ts
+++ app/src/hooks.ts
@@ -1,10 +1,14 @@
 import { parseToken } from '$lib/jwt'
 
-const getCookies = ({ headers : { cookie } }) => Object.fromEntries(cookie.split(';').map(c=>c.split('=').map(c=>c.trim())))
+const getCookies = ({headers: {cookie}}) => Object.fromEntries(
+  cookie
+    ? cookie.split(";")
+            .map((c) => c.split("=").map((c2) => c2.trim()))
+    : []
+);
 
 /** @type {import('@sveltejs/kit').GetSession} */
 export function getSession(request) {
-
   const { token } = getCookies(request)
   const user = parseToken(token)
 
--- app/src/lib/graphql.ts
+++ app/src/lib/graphql.ts
@@ -1,10 +1,13 @@
 import { browser } from '$app/env';
 import { writable } from 'svelte/store'
 import { gql, GraphQLClient } from 'graphql-request'
+import { GRAPHQL_ENDPOINT } from '$lib/config'
+
+console.log("has configured endpoint:", GRAPHQL_ENDPOINT)
 
 export { gql }
 export const user = writable(null)
-export const client = new GraphQLClient('http://graphql.wolbodo/v1/graphql')
+export const client = new GraphQLClient(GRAPHQL_ENDPOINT)
 
 const SESSION_USER = 'user'
 
