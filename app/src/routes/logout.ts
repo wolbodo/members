b6c604e29bcb8462af4f7dd9551a3f2885e27149
Dexter <a.esselink@gmail.com>
2021-05-31 12:18:59 +0200

    Fix logout

--- app/src/routes/logout.ts
+++ app/src/routes/logout.ts
@@ -3,7 +3,8 @@ import type { Locals } from '$lib/types';
 
 export const get: RequestHandler<Locals, FormData> = () => ({
   status: 200,
+  body: null,
   headers: {
-    'Set-Cookie': `Set-Cookie: token=gone; expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly`
+    'Set-Cookie': 'token=gone; expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly'
   }
 })
