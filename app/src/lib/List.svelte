f667a33cce33292cdcfead125ac1305dcc621b67
Dexter <a.esselink@gmail.com>
2021-05-31 17:59:03 +0200

    Made the list prettier

--- app/src/lib/List.svelte
+++ app/src/lib/List.svelte
@@ -1,42 +1,64 @@
 <script lang="ts">
   import { session } from '$app/stores';
 	import { client, gql } from '$lib/graphql'
+
+  type Column = {
+    label: string;
+    fields: string[];
+    href?: (object) => string;
+    format?: (object) => string;
+  }
+  let columns : Column[] = [
+    { label: 'Name', fields: ['name'], href: ({ name }) => `m/${name.toLowerCase()}`},
+    { label: 'Email', fields: ['email'] },
+    { label: 'Phone', fields: ['phone'] },
+    {
+      label: 'Full name',
+      fields:['firstname', 'lastname'],
+      format: ({ firstname, lastname }) => `${firstname??''} ${lastname??''}`.trim() },
+  ]
 </script>
 
 <section class='actions'>
-
   {#if $session.user?.roles && $session.user.roles.includes('board')}
-    <a href='create'>Create</a>
+    <a class='button accent' href='create'>Create</a>
+    <a class='button accent' href='create'>Create</a>
+    <a class='button accent' href='create'>Create</a>
+    <a class='button accent' href='create'>Create</a>
   {/if}
 </section>
 
 <table>
   <thead>
-    <td>
-      Name
-    </td>
-    <td>
-      Email
-    </td>
-    <td>
-      Phone
-    </td>
+    {#each columns as { label }}
+      <th>{label}</th>
+    {/each}
   </thead>
   <tbody>
     {#await client.request(gql`
       {
-        people: auth_person{
-          name email phone
+        people: auth_person (order_by:{id:asc}) {
+          ${columns.flatMap(({ fields }) => fields).join(' ')}
         }
       }
     `)}
       <tr class="ssc-line"></tr>
     {:then { people }} 
-      {#each people as { name, email, phone}}
+      {#each people as person}
         <tr>
-          <td><a href='m/{name.toLowerCase()}'>{name}</a></td>
-          <td>{email}</td>
-          <td>{phone}</td>
+          {#each columns as { fields, href, format }}
+          <td>
+            {#if href}
+              <a href={href(person)}>
+                {format ? format(person)
+                        : person[fields[0]] ?? ''}
+              </a>
+            {:else}
+              {format ? format(person)
+                      : person[fields[0]] ?? ''}
+            {/if}
+          </td>
+          {/each}
         </tr>
       {:else}
         <tr>
@@ -47,3 +69,40 @@
   </tbody>
 </table>
 
+<style>
+  .actions {
+    width: 100%;
+    display:flex;
+  }
+  .actions > * {
+    margin: .5rem;
+  }
+  table {
+    border-collapse: collapse;
+    width: 100%;
+  }
+  th {
+    background-color: var(--tertiary-color);
+    position: sticky;
+    top: 0;
+    font-weight: bold;
+    border-bottom: 1px solid var(--accent-color);
+    text-align: left;
+  }
+  th, td {
+    line-height: 1.4;
+    padding-right: .5rem;
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+  }
+  th:first-child, td:first-child {
+    padding-left: 1rem;
+  }
+  tr {
+    background-color: var(--pure-white);
+    border-bottom: 1px solid var(--primary-color);
+  }
+  tr:hover {
+    background-color: var(--secondary-color);
+  }
+</style>
