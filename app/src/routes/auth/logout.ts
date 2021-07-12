4b5050861aea545c57e001839d091603c3cfb7d1
Dexter <a.esselink@gmail.com>
2021-07-12 17:12:17 +0200

    Adding cookie domain

--- app/src/routes/auth/logout.ts
+++ app/src/routes/auth/logout.ts
@@ -1,11 +1,11 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import type { Locals } from '$lib/types';
-import { cookie } from './_cookie'
+import { setCookie } from '$lib/cookies';
 
 export const post: RequestHandler<Locals, FormData> = () => ({
   status: 200,
   body: null,
   headers: {
-    'Set-Cookie': cookie()
+    'Set-Cookie': cookie('token', '', { expires: new Date(1970) })
   }
 })
