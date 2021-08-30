93e7d63e166144f5fe6f6a19497689fda08cb380
Dexter <a.esselink@gmail.com>
2021-08-30 22:00:29 +0200

    AllowCreds

--- app/src/routes/auth/mercure.ts
+++ app/src/routes/auth/mercure.ts
@@ -24,6 +24,7 @@ export const get: RequestHandler<Locals, FormData> = () => {
     body: null,
     headers: {
       'Access-Control-Allow-Origin': 'https://votes.wolbodo.nl',
+      'Access-Control-Allow-Credentials': true,
       'Set-Cookie': setCookie('mercureAuthorization', token, { Domain: MERCURE_DOMAIN,  SameSite: 'None' })
     }
   }
