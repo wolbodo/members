2098d9013d37c05f7b76afbe9f443978e01bf8c1
Dexter <dexter@dexterlabs.nl>
2021-11-23 23:40:55 +0100

    Init houdini

---
+++ houdini.config.js
@@ -0,0 +1,10 @@
+/** @type {import('houdini').ConfigFile} */
+const config = {
+	schemaPath: './schema.graphql',
+	sourceGlob: 'src/**/*.svelte',
+	module: 'esm',
+	framework: 'kit',
+	apiUrl: 'http://graphql.wolbodo/v1/graphql'
+}
+
+export default config
--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,31 @@
 import preprocess from 'svelte-preprocess';
+import houdini from 'houdini-preprocess';
+import path from 'path';
 import node from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: [preprocess(), houdini()],
 	
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: node()
+		adapter: node(),
+
+        vite: {
+			server: {
+				fs: {
+					allow: ['.']
+				}
+			},
+            resolve: {
+                alias: {
+                    $houdini: path.resolve('.', '$houdini')
+                }
+            }
+        }
 	}
 };
 
