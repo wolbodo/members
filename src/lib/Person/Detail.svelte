129b40e5cf7ee3bf710b435b00c873ce83553ea8
Dexter <dexter@dexterlabs.nl>
2021-11-24 22:23:59 +0100

    prettier

--- src/lib/Person/Detail.svelte
+++ src/lib/Person/Detail.svelte
@@ -1,129 +1,128 @@
-<script lang='ts'>
-  import { onMount, createEventDispatcher } from 'svelte'
-  import { datetime } from '$lib/format'
-  import { client, gql } from '$lib/graphql'
-  import { Input, RoleSelector, mutation as gqlMutation } from '$lib/Form'
+<script lang="ts">
+	import { onMount, createEventDispatcher } from 'svelte';
+	import { datetime } from '$lib/format';
+	import { client, gql } from '$lib/graphql';
+	import { Input, RoleSelector, mutation as gqlMutation } from '$lib/Form';
 
-  const dispatch = createEventDispatcher();
+	const dispatch = createEventDispatcher();
 
-  let error
-  export let mutation, variables
-  export let person
-  export let role = 'board'
-  export let permissions = {view: [], edit: []}
-  let roles
+	let error;
+	export let mutation, variables;
+	export let person;
+	export let role = 'board';
+	export let permissions = { view: [], edit: [] };
+	let roles;
 
-  export let fieldSet: string[][] = [
-    [ 'name', ],
-    [ 'firstname', 'lastname', 'email', 'phone'],
-    [ 'address', 'zipcode', 'city', 'country'],
-    [ 'bankaccount', 'key_code'],
-    [ 'roles',],
-    ['password'],
-    [ 'note'],
-    [ 'created', 'modified', 'id' ],
-  ]
+	export let fieldSet: string[][] = [
+		['name'],
+		['firstname', 'lastname', 'email', 'phone'],
+		['address', 'zipcode', 'city', 'country'],
+		['bankaccount', 'key_code'],
+		['roles'],
+		['password'],
+		['note'],
+		['created', 'modified', 'id']
+	];
 
-  onMount(async () => {
-    const data =  await client.request(gql`
-        query getRoles {
-          roles: auth_person_role(distinct_on:role) {
-            role
-          }
-        }
-      `
-    )
-    roles = data.roles.map(({ role }) => role)
-  })
+	onMount(async () => {
+		const data = await client.request(gql`
+			query getRoles {
+				roles: auth_person_role(distinct_on: role) {
+					role
+				}
+			}
+		`);
+		roles = data.roles.map(({ role }) => role);
+	});
 
+	$: fieldOptions = {
+		name: {
+			class: 'wide'
+		},
+		firstname: {},
+		lastname: {},
+		email: { type: 'email' },
+		phone: { type: 'tel' },
+		address: {},
+		city: {},
+		country: {},
+		bankaccount: {},
+		key_code: {},
+		id: { readonly: true },
+		created: { readonly: true, format: datetime },
+		modified: { readonly: true, format: datetime },
+		password: { type: 'password' },
+		note: { type: 'textarea' },
+		roles: { type: RoleSelector, options: roles }
+	};
 
-  $: fieldOptions = {
-    name: {
-      class: 'wide'
-    },
-    firstname: { },
-    lastname: { },
-    email: { type: 'email' },
-    phone: { type: 'tel' },
-    address: { },
-    city: { },
-    country: { },
-    bankaccount: { },
-    key_code: { },
-    id: { readonly: true },
-    created: { readonly: true, format: datetime },
-    modified: { readonly: true, format: datetime  },
-    password: { type: 'password' },
-    note: { type: 'textarea' },
-    roles: { type: RoleSelector, options: roles }
-  }
-
-  $: fieldInfo = fieldSet
-                  .map(fields =>
-                    fields.filter(field => permissions.view.includes(field))
-                      .map(name => {
-                        const field = {
-                          name,
-                          readonly: true,
-                          value: person && person[name],
-                          ...fieldOptions[name]
-                        }
-                        if (permissions.edit.includes(name)) {
-                          delete field.readonly
-                        }
-                        return field
-                      })
-                  )
-                  .filter(fields => fields.length)
-
+	$: fieldInfo = fieldSet
+		.map((fields) =>
+			fields
+				.filter((field) => permissions.view.includes(field))
+				.map((name) => {
+					const field = {
+						name,
+						readonly: true,
+						value: person && person[name],
+						...fieldOptions[name]
+					};
+					if (permissions.edit.includes(name)) {
+						delete field.readonly;
+					}
+					return field;
+				})
+		)
+		.filter((fields) => fields.length);
 </script>
 
-<form use:gqlMutation={{
-  role,
-  mutation, variables,
-  error: (_, err) => error = err.toString(),
-  result: data => dispatch('save', data)
-}}>
+<form
+	use:gqlMutation={{
+		role,
+		mutation,
+		variables,
+		error: (_, err) => (error = err.toString()),
+		result: (data) => dispatch('save', data)
+	}}
+>
+	{#each fieldInfo as group}
+		<section class="grid">
+			{#each group as input}
+				<Input {...input} />
+			{/each}
+		</section>
+	{/each}
 
-  {#each fieldInfo as group }
-    <section class='grid'>
-      {#each group as input}
-        <Input {...input} />
-      {/each}
-    </section>
-  {/each}
+	{#if error}
+		<small>{error}</small>
+	{/if}
 
-  {#if error}
-    <small>{error}</small>
-  {/if}
-
-  {#if permissions.edit.length}
-    <button type='submit'>Submit</button>
-  {/if}
+	{#if permissions.edit.length}
+		<button type="submit">Submit</button>
+	{/if}
 </form>
 
 <style>
+	form {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	button {
+		background: var(--accent-color);
+	}
 
-  form {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-  button {
-    background: var(--accent-color);
-  }
+	section {
+		min-width: 600px;
 
-  section {
-    min-width: 600px;
+		display: grid;
+		grid-template-columns: repeat(2, auto);
+		grid-column-gap: 1rem;
+	}
 
-    display: grid;
-    grid-template-columns: repeat(2,auto);
-    grid-column-gap: 1rem;
-  }
-
-  @media screen and (max-width: 500px) {
-      section {
-        grid-template-columns: auto;
-      }
-  }
-  </style>
+	@media screen and (max-width: 500px) {
+		section {
+			grid-template-columns: auto;
+		}
+	}
+</style>
