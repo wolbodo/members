a2c3a02760a0f852ac2b0792b1a227c442b296e6
Dexter <dexter@dexterlabs.nl>
2021-12-14 15:25:08 +0100

    Fixing dev cookies

--- src/routes/auth/logout.ts
+++ src/routes/auth/logout.ts
@@ -1,11 +1,23 @@
 import type { RequestHandler } from '@sveltejs/kit';
+
+import { dev } from '$app/env';
+
 import type { Locals } from '$lib/types';
 import { setCookie } from '$lib/cookies';
 
-export const post: RequestHandler<Locals, FormData> = () => ({
-  status: 200,
-  body: null,
-  headers: {
-    'Set-Cookie': setCookie('token', '', { expires: new Date(1970), Domain: 'wolbodo.nl' })
+const COOKIE_DOMAIN = process.env.COOKIE_DOMAIN || 'wolbodo.nl'
+
+export const post: RequestHandler<Locals, FormData> = () => {
+  console.log("Domain:", COOKIE_DOMAIN)
+  return {
+    status: 200,
+    body: null,
+    headers: {
+      'Set-Cookie': setCookie('token', '', {
+        expires: new Date(1970),
+        secure: !dev,
+        Domain: COOKIE_DOMAIN
+      })
+    }
   }
-})
+}
--- src/routes/logout.svelte
+++ src/routes/logout.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
-	import { onMount } from 'svelte'
-	
-	import { goto } from '$app/navigation'
-	import { session } from '$app/stores'
-	
-	onMount(async () => {
-		await fetch('/auth/logout', { method: 'post' })
+	import { onMount } from 'svelte';
 
-		console.log("Redirecting")
-		$session = {}
-		goto('/login')
-	})
+	import { goto } from '$app/navigation';
+	import { session } from '$app/stores';
+
+	onMount(async () => {
+		console.log('Logging out');
+		await fetch('/auth/logout', { method: 'post' });
+		console.log('Logging out ok');
+
+		console.log('Redirecting');
+		$session = {};
+		goto('/login');
+	});
 </script>
