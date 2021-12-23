af5342ce783e82ec95157e9d0f0ce1f37e9b92fe
Dexter <dexter@dexterlabs.nl>
2021-12-23 15:16:22 +0100

    Fix password reset

--- src/lib/mail/index.ts
+++ src/lib/mail/index.ts
@@ -1,15 +1,24 @@
+import { gql, GraphQLClient } from 'graphql-request'
+
+import { serverToken } from '$lib/jwt'
+import { GRAPHQL_ENDPOINT } from '$lib/config'
+
+const client = new GraphQLClient(GRAPHQL_ENDPOINT)
 
 export { default as templates } from './templates'
-export const send = (personId, template, data) => client.request(gql`
-  mutation sendMail($template:String!, $personId:Int!, $data:jsonb = {}) {
-    insert_mail_entries_one(object:{
-      data:$data,
-      template:$template,
-      person_id:$personId
-    }) {
-      id
+export const send = (personId, template, data) => {
+  client.setHeader('authorization', `Bearer ${serverToken('send-mail')}`)
+  client.request(gql`
+    mutation sendMail($template:String!, $personId:Int!, $data:jsonb = {}) {
+      insert_mail_entries_one(object:{
+        data:$data,
+        template:$template,
+        person_id:$personId
+      }) {
+        id
+      }
     }
-  }
-  `, {
+    `, {
     personId, template, data
   })
+}
