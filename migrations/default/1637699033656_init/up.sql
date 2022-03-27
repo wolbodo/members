caf64ea29e84ef05bcbf252c9424c3e61e8b7910
Dexter <dexter@dxlb.nl>
2022-03-27 21:52:19 +0200

    Seems good

--- migrations/default/1637699033656_init/up.sql
+++ migrations/default/1637699033656_init/up.sql
@@ -13,6 +13,31 @@ CREATE TYPE mail.status AS ENUM (
     'sent',
     'error'
 );
+
+
+CREATE FUNCTION public.jsonb_diff(new jsonb, old jsonb) RETURNS jsonb
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+  result JSONB;
+  v RECORD;
+BEGIN
+   result = new;
+   FOR v IN SELECT * FROM jsonb_each(old) LOOP
+     IF result @> jsonb_build_object(v.key,v.value)
+        THEN result = result - v.key;
+     ELSIF result ? v.key THEN CONTINUE;
+     ELSE
+        result = result || jsonb_build_object(v.key,'null');
+     END IF;
+   END LOOP;
+   RETURN result;
+END;
+$$;
+
+
+ALTER FUNCTION public.jsonb_diff(new jsonb, old jsonb) OWNER TO postgres;
+
 CREATE FUNCTION auth.change_trigger() RETURNS trigger
     LANGUAGE plpgsql
     AS $$
