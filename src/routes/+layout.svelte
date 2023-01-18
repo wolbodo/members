d2428a1d20ca92c50ef5f30612f364e8c7a58f77
Dexter <dexter@dxlb.nl>
2023-01-19 00:26:09 +0100

    Initial refactor to v1

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,4 @@
-<script context="module">
+<script context="module" lang='ts'>  
 	import env from '../environment';
 	import { setEnvironment } from '$houdini';
 
--- svelte.config.js
+++ svelte.config.js
@@ -1,30 +1,30 @@
-import preprocess from 'svelte-preprocess';
 import houdini from 'houdini-preprocess';
 import path from 'path';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [preprocess(), houdini()],
-	
+	preprocess: [vitePreprocess(), houdini()],
+
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		adapter: adapter(),
 
-        vite: {
+		vite: {
 			server: {
 				fs: {
 					allow: ['.']
 				}
 			},
-            resolve: {
-                alias: {
-                    $houdini: path.resolve('.', '$houdini')
-                }
-            }
-        }
+			resolve: {
+				alias: {
+					$houdini: path.resolve('.', '$houdini')
+				}
+			}
+		}
 	}
 };
 
