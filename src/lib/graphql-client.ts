9c93f8d5cd6a5700eb4bfcc0734968cd61b2edf2
Dexter <dexter@dxlb.nl>
2023-03-04 11:59:09 +0100

    Left it somewhere

--- src/lib/graphql-client.ts
+++
@@ -1,6 +0,0 @@
-import { GraphQLClient } from 'graphql-request'
-import { GRAPHQL_ENDPOINT } from '$lib/config'
-export { gql } from 'graphql-request'
-
-
-export const client = new GraphQLClient(GRAPHQL_ENDPOINT)
--- src/routes/mail/+page.svelte
+++ src/routes/mail/+page.svelte
@@ -1,24 +1,13 @@
 <script lang="ts">
-	import { query, graphql, AllMail } from '$houdini';
+	import type { PageData } from './$houdini';
 
 	import { datetime } from '$lib/format';
 	import Table from '$lib/Table.svelte';
 	import { searchValue, filterFields } from '$lib/Header/index.svelte';
 
-	const { data, loading } = query<AllMail>(graphql`
-		query AllMail {
-			mails: mail_entries(order_by: { created: desc }, limit: 100) {
-				id
-				status
-				person {
-					name
-					email
-				}
-				template
-				created
-			}
-		}
-	`);
+	export let data: PageData;
+
+	$: ({ AllMail } = data);
 </script>
 
 <h1>Mails</h1>
@@ -32,10 +21,10 @@
 			<th>Time</th>
 		</tr>
 	</thead>
-	{#if $loading}
+	{#if $AllMail.fetching}
 		<tr><td colspan="4">Loading</td></tr>
-	{:else}
-		{#each $data.mails.filter( (mail) => filterFields($searchValue, mail.person.name, mail.person.email, mail.status, mail.template) ) as { status, person, template, created }}
+	{:else if $AllMail.data}
+		{#each $AllMail.data.mails.filter( (mail) => filterFields($searchValue, mail.person.name, mail.person.email, mail.status, mail.template) ) as { status, person, template, created }}
 			<tr>
 				<td>{status}</td>
 				<td><a href="mailto:{person.email}">{person.name}</a></td>
@@ -43,5 +32,7 @@
 				<td>{datetime(created)}</td>
 			</tr>
 		{/each}
+	{:else}
+		<tr><td colspan="4">No data</td></tr>
 	{/if}
 </Table>
