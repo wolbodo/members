e2ca18f0c48d096dae39813ae83bb8b33f0b05f5
Dexter <dexter@dxlb.nl>
2023-03-07 13:37:37 +0100

    Setting up shizzle

--- src/lib/mail/index.ts
+++ src/lib/mail/index.ts
@@ -1,24 +1,17 @@
-import { gql, GraphQLClient } from 'graphql-request'
+import { gql, GraphQLClient } from 'graphql-request';
 
-import { serverToken } from '$lib/jwt'
-import { GRAPHQL_ENDPOINT } from '$lib/config'
+import { serverToken } from '$lib/jwt';
+import { GRAPHQL_ENDPOINT } from '$lib/config';
+import { SendMailStore } from '$houdini';
 
-const client = new GraphQLClient(GRAPHQL_ENDPOINT)
+const client = new GraphQLClient(GRAPHQL_ENDPOINT);
 
-export { default as templates } from './templates'
+export { default as templates } from './templates';
 export const send = (personId, template, data) => {
-  client.setHeader('authorization', `Bearer ${serverToken('send-mail')}`)
-  client.request(gql`
-    mutation sendMail($template:String!, $personId:Int!, $data:jsonb = {}) {
-      insert_mail_entries_one(object:{
-        data:$data,
-        template:$template,
-        person_id:$personId
-      }) {
-        id
-      }
-    }
-    `, {
-    personId, template, data
-  })
-}
+	client.setHeader('authorization', `Bearer ${serverToken('send-mail')}`);
+	client.request(gql``, {
+		personId,
+		template,
+		data
+	});
+};
