6985aa601f82de9bfb608964610176194d01d1ea
Dexter <dexter@dxlb.nl>
2023-06-13 21:57:05 +0200

    Needs fields in queries

--- houdini.config.js
+++ houdini.config.js
@@ -13,9 +13,9 @@ const config = {
 	logLevel: 'full',
 
 	watchSchema: {
-		url: 'http://graphql.wolbodo/v1/graphql',
+		url: 'http://graphql.wolbodo.nl/v1/graphql',
 		headers: {
-			'x-hasura-admin-secret': 'secret'
+			'x-hasura-admin-secret': '4P5eRndatPN5bWq6GOUC0FCU10H0TaBKHAEEF8cTCHlb771Y4ekzshYcj400'
 		},
 		interval: 20000
 	},
